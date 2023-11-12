07c77970c71aeb8c60b74e0110ab441d4802cbc2
mfoster0 <63725574+mfoster0@users.noreply.github.com>
2023-11-12 06:09:34 +0000

    Merged core codebase
    Added in the node files. Now using the latest app.js, map.js versions which have:
     - dynamic, configurable interactions with the database
    - more error handling
    - UI now using asynch functionality

---
+++ CASA CHEMS_website_final/web-mapdisplay/app.js
@@ -0,0 +1,198 @@
+//###########################################################################################
+//###########################################################################################
+// This code is set up to serve the webpages from CASA-CHEMS Group Project
+// It serves the main tree map and related insights page
+// It is responsible for the communication with the database via the connection pool
+// It performs validation on the requests and limits the number of requests from an IP
+// The fields returned in the queries executed can be configured in the ./config file 
+// Code written by M. Foster
+//###########################################################################################
+//###########################################################################################
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const app = express();
+
+//using environment variable __dirname - absolute path of dir of executing file
+app.use(express.static(__dirname));
+
+// Serve the mapjson directory as a static folder
+app.use('/mapjson', express.static(path.join(__dirname, 'mapjson')));
+app.use('/js', express.static(path.join(__dirname, 'js')));
+
+const limiterLib = require('express-rate-limit');
+
+//set request rate limits
+const limiter = limiterLib.rateLimit({
+	windowMs: 10 * 1000, // 10 secs
+	limit: 10, // Limit each IP to 10 requests per `window` 
+	standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+})
+
+// Apply the rate limiting middleware to all requests.
+app.use(limiter)
+
+//start up create a connection pool with the database
+const config = require('./config'); // Import config
+const dbConfig = config.databasePool; //get the DB config
+const port = config.server_port; // change port as needed
+const host = 'casa0017.cetools.org';
+
+const mysql = require('mysql');
+const { log } = require('console');
+//create pool of connections to manage load
+const pool = mysql.createPool(dbConfig);
+let sqlCols = "";
+
+//this is used to provide a configurable set of data for the client ward data container
+//the config file contains the available columns, aliases and formatting of data requested from the db
+//the 'Use' attibute is used to switch on or off the retreival of that column
+//get columns from config and iterate through to build the extra sql
+try{
+    Object.entries(config.wardQueryCols).forEach(([columnName, columnConfig]) => {
+      if (columnConfig.Use) {
+        sqlCols += ", " + columnConfig.Text;
+      }
+    });
+}
+catch(error){
+  console.log("Error loading wardQueryCols");
+  logIssue("Error loading wardQueryCols");
+}
+
+
+// root route that serves your HTML file to client
+app.get('/', (req, res) => {
+  try{
+    ///console.log("__dirname: " + __dirname);
+    logMsg("Received root request");
+    res.sendFile(path.join(__dirname, 'Map page.html'));
+  }
+  catch(error){
+    console.log(`Error loading root on get request: ${error}`);
+    logIssue(`Error loading root on get request: ${error}`);
+    return 0;
+  }
+  return 1;
+});
+
+// this char check code was sourced from https://javascript.plainenglish.io/check-if-string-is-alphanumeric-in-javascript-e325caa3ee
+// no revisions were required for this string check, just try catch
+function isAlphanumeric(str) {
+  try{
+    return /^[a-zA-Z0-9]+$/.test(str);
+  }
+  catch(error){
+    console.log(`Error checking alphanumerics: ${error}`);
+    logIssue(`Error checking alphanumerics: ${error}`);
+  }
+  return false;
+}
+
+//used for front end fetch request
+app.get('/getWardData', (req, res) => {
+  try{
+    logMsg("Received getWardData request: " + req.query.wardCode);
+    //console.log("Received getWardData request: " + req.query.wardCode);
+
+    //use ward code
+    const wardCode = req.query.wardCode;
+    //console.log('Received wardCode:', wardCode);
+
+    // restrict wardcode request to 9 chars and all must be alphanumeric
+    // this is basic validation and helps prevent client SQL injection
+    if (wardCode.length != 9 || ! isAlphanumeric(wardCode)){
+        const errMsg = `Bad request in getWardData: ${wardCode}`;
+        console.error(errMsg);
+        logIssue(errMsg);
+        res.status(400);
+        return;
+    }
+
+    let sql = `select p.borough as Borough` + sqlCols + ` FROM ward_geo_json_attributes as a  LEFT JOIN public_open_space as p ON a.wardcode = p.ward_code  LEFT JOIN green_ward_cover as g ON a.wardcode = g.ward_code where a.wardcode = '` + wardCode + `';`;
+    
+    // console.log(sql);
+
+    pool.query(sql, (err, results) => {
+      if (err) {
+        console.error('Error connecting to the database:', err);
+        logIssue('Error connecting to the database:', err);
+        res.status(500);
+        return;
+      }
+      //console.log('Connected to the MySQL database');
+      //console.log("DB Conn Status: " + pool.status);
+      
+      res.status(200).json(results);
+      
+    });
+  }
+  catch(error){
+    console.log(`Error checking alphanumerics: ${error}`);
+    logIssue(`Error checking alphanumerics: ${error}`);
+    res.status(500);
+    return 0;
+  }
+  return 1;
+});
+
+//start listening
+app.listen(port, () => {
+  console.log(`Server running on http://${host}:${port}`);
+  logMsg(`Server is running on http://${host}:${port}`);
+  return 1;
+});
+
+
+// Write to daily issues file
+function logIssue(issue) {
+  const currentDate = new Date();
+  //get the date and time
+  const curDateTime = currentDate.toISOString();
+
+  try{
+    // Get date with no spaces or time for file name
+    const curDate = currentDate.toISOString().split('T')[0].replace(/-/g, '');
+    
+    // Build the log file path
+    const logFilePath = `logs/issue-log-${curDate}.txt`;
+
+    // create or append to log file
+    const logMessage = `[${curDateTime}] ${issue}\n`;
+    fs.appendFileSync(logFilePath, logMessage);
+
+    console.log(`Issue logged: ${logFilePath}`);
+  }
+  catch(error){
+    console.log(`Error logging issue: [${curDateTime}] ${error}\n`);
+    return 0;
+  }
+  return 1;
+}
+
+// Write to daily activity log file
+function logMsg(msg) {
+  const currentDate = new Date();
+  //get the date and time
+  const curDateTime = currentDate.toISOString();
+
+  try{
+    // Get date with no spaces or time for file name
+    const curDate = currentDate.toISOString().split('T')[0].replace(/-/g, '');
+    
+    // Build the log file path
+    const logFilePath = `logs/app-log-${curDate}.txt`;
+
+    // create or append to log file
+    const logMessage = `[${curDateTime}] ${msg}\n`;
+    fs.appendFileSync(logFilePath, logMessage);
+
+    //console.log(`Msg logged: ${logFilePath}`);
+  }
+  catch(error){
+    console.log(`Error logging message: [${curDateTime}] ${error}\n`);
+    return 0;
+  }
+  return 1;
+}
--- CASA CHEMS_website_final/web-mapdisplay/js/map.js
+++ CASA CHEMS_website_final/web-mapdisplay/js/map.js
@@ -1,5 +1,22 @@
 //Mapbox API TOKEN
-const API_TOKEN_Mapbox = 'pk.eyJ1IjoidnNpZ25vIiwiYSI6ImNrc2IxcjV0ejAyNnQydXFxdG14Nnk4ZHcifQ.2YkWTQsNvu4cFyJWsHKSiw';
+//const mapsecrets = require('/mapsecrets.js'); // Import the mapsecrets file
+
+// Access the secret
+//const API_TOKEN_Mapbox = mapsecrets.API_TOKEN_MB;
+
+API_TOKEN_Mapbox = '';
+//retreive API token
+fetch(`/getApiKey`)
+.then(response => response.json())
+.then(data => {
+    API_TOKEN_Mapbox = data;
+    console.log("data: " + data);
+    console.log("api_tok:" + API_TOKEN_Mapbox)
+});
+console.log("API Token: " + API_TOKEN_Mapbox);
+
+API_TOKEN_Mapbox = 'pk.eyJ1IjoidnNpZ25vIiwiYSI6ImNrc2IxcjV0ejAyNnQydXFxdG14Nnk4ZHcifQ.2YkWTQsNvu4cFyJWsHKSiw';
+
 mapboxgl.accessToken = API_TOKEN_Mapbox;
 
 //Initialize map view
@@ -89,11 +106,17 @@ map.on('load', () => {
         'filter': ['in', 'wardname', '']
     }, 'housenum-label');
 
+    
     //Map click event
     map.on('click', 'area-tree-layer', (e) => {
-        //Display area information after clicking
-        var description = ("Area name: " + e.features[0].properties.wardname + '<br>' + "Coverage: " + e.features[0].properties.percancov + " %" + '<br>' + "survyear: " + e.features[0].properties.survyear + '<br>' + "warea: "+e.features[0].properties.warea+" m<sup>2</sup>");
-        document.getElementById("popup").innerHTML = description;
+        var wardCode = e.features[0].properties.wardcode;
+        
+        //Build the popup text
+        var description = ("Area Name: " + e.features[0].properties.wardname + '<br>' + "Coverage: " + Number(e.features[0].properties.percancov).toFixed(2) + " %" + '<br>' + "Survey Year: " + e.features[0].properties.survyear + '<br>' + "Ward Area: "+ Number(e.features[0].properties.warea).toFixed(2) + " m<sup>2</sup>" + '<br>');
+        //console.log(description);
+
+        const fieldValue = fetchWardData(wardCode, description);
+        //console.log("FV:" + fieldValue);
 
         //Find the current area based on the mouse click position and display the highlighted layer
         //5px area around
@@ -121,6 +144,7 @@ map.on('load', () => {
         map.fitBounds(bounds, {
             padding: 20
         });
+
 
     });
 
@@ -180,6 +204,8 @@ map.addControl(geocoder);
 var x_data = [];
 var y_data = [];
 var all_data = [[],[]];
+var all_wards = {};
+var ward_coord = {};
 //Read geojson
 $.ajax({
     url: "./mapjson/UK_canapy.geojson",
@@ -190,12 +216,92 @@ $.ajax({
         for (var i = 0; i < data.features.length; i++) {
             x_data.push(data.features[i].properties.percancov);
             y_data.push(data.features[i].properties.wardname);
+            all_wards[data.features[i].properties.wardname] = data.features[i].properties.wardcode;
+            ward_coord[data.features[i].properties.wardname] = getCoords(data.features[i].geometry.coordinates);
         }
         displaylist(data);
     },
 });
 
+//just grab the first coord for the geometry
+function getCoords(geom){
+    var startChars = "-0123456789";
+    var validChars = startChars + ", ";
+    var geo = JSON.stringify(geom);
+    var textLen = geo.length;
+    var coords = "";
+    var i = 0;
 
+    
+    while (i < textLen){
+        i += 1;
+        if (coords.length == 0) {
+            //get first char
+            if (startChars.includes(geo[i]) ){
+                coords = geo[i];
+            } //else do nothing
+        } else{
+            if (validChars.includes(geo[i])){
+                coords += geo[i];
+            }
+            if (geo[i] == ']'){
+                coords = "[" + coords + "]";
+                return coords;
+            }
+        }
+    } 
+    return coords;
+}
+
+//using async function to display the data from the database when returned,
+// otherwise execution continues before data is received
+async function fetchWardData(wardCode, description) {
+    
+    try {
+            const response = await fetch(`/getWardData?wardCode=${wardCode}`);
+            //use await to process when node returns the data from the DB
+            const data = await response.json();
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+
+            // Continue with the data once it's available
+            var dataFromDB = "";
+            //data is nested so need to iterate through it
+            //create a stack to pop off each data record
+            const stack = [{ obj: data, prefix: '' }];
+
+            while (stack.length > 0) {
+                //pop the next record  
+                const { obj, prefix } = stack.pop();
+        
+                for (const key in obj) {
+                    //if key is an object, it's not the actual record
+                    if (typeof obj[key] === 'object') {
+                        stack.push({ obj: obj[key], prefix: `${prefix}${key}.` });
+                    } else {
+                        const attributeName = `${key}`;
+                        const attributeValue = obj[key];
+                        //console.log(`Attribute Name: ${attributeName}, Attribute Value: ${attributeValue}`);
+                        dataFromDB += attributeName + ": " + attributeValue + "<BR>";
+                    }
+                }
+            }
+            //if query returns no records, inform user
+            if (dataFromDB.length == 0){  
+                dataFromDB =  "<BR>Supplemental data not available for this ward";
+            }
+            //Display area information after clicking
+            document.getElementById("popup").innerHTML = description + dataFromDB;
+            return 1;
+
+    } catch (error) {
+        //if this errors, just show the data from the GeoJSON flat file
+        document.getElementById("popup").innerHTML = description + "<BR>Supplemental data not available for this ward";  
+        console.error('Error:', error);
+        return 0;
+    }
+}
 
 
 
@@ -205,7 +311,7 @@ var displaychart = echarts.init(document.getElementById("barchart"));
 function displaylist(data) {
     displaychart.setOption({
         title: {
-            text: 'Tree coverage list',
+            text: 'Tree Coverage List',
             x: 'center',
             y: 'top',
         },
@@ -259,11 +365,28 @@ function displaylist(data) {
 }
 //Click the bar to zoom to the area location
 displaychart.on('click', function (params) {
-    //Filter geojson data
+    //Filter geojson data   
     var features = map.querySourceFeatures('area-tree', {
         sourceLayer: 'area-tree-layer',
         filter: ["==", 'wardname', params.name]
     });
+  
+    //if features undefined then zoom out and try again
+    if (typeof features !== 'undefined'){
+        //console.log("Zooming out to " + params.name);
+        //zoom out first
+        map.flyTo({center: [INITIAL_VIEW_STATE.longitude, INITIAL_VIEW_STATE.latitude], essential: false, zoom:10});
+ 
+        //Filter geojson data
+        features = map.querySourceFeatures('area-tree', {
+            sourceLayer: 'area-tree-layer',
+            filter: ["==", 'wardname', params.name]
+        });
+
+        //if zooming out, clear the popup data
+        document.getElementById("popup").innerHTML = "";
+    }
+
     //Zoom to location
     var coordinates = features[0].geometry.coordinates[0];
     var bounds = coordinates.reduce(function (bounds, coord) {
@@ -272,15 +395,24 @@ displaychart.on('click', function (params) {
     map.fitBounds(bounds, {
         padding: 20
     });
+    
+    //get ward code
+    var wardCode = all_wards[params.name];
+
     //Show area information
-    var description = ("Area name: " + features[0].properties.wardname + '<br>' + "Coverage: " + features[0].properties.percancov + " %"+ '<br>' + "survyear: " +features[0].properties.survyear + '<br>' + "warea: "+features[0].properties.warea+" m<sup>2</sup>");
+    var description = ("Area name: " + features[0].properties.wardname + '<br>' + "Coverage: " + Number(features[0].properties.percancov).toFixed(2) + " %"+ '<br>' + "survyear: " +features[0].properties.survyear + '<br>' + "warea: "+ Number(features[0].properties.warea).toFixed(2) +" m<sup>2</sup>");
+    
     document.getElementById("popup").innerHTML = description;
     map.setFilter('area-tree-layer-highlighted', ['in', 'wardname', params.name]);
+
+    const fieldValue = fetchWardData(wardCode, description);
 })
+
 //When the mouse is hovering over bar, highlighted area on map
 displaychart.on('mouseover',function(params){
     map.setFilter('area-tree-layer-highlighted', ['in', 'wardname', params.name]);
 })
+
 //button to hide the bar chart
 function displaybarcharts() {
     var chart = document.getElementById("barchart111");
