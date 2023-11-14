5a8465dfc14542719f49dcea7b5f4cd7ee315f66
mfoster0 <63725574+mfoster0@users.noreply.github.com>
2023-11-14 06:55:30 +0000

    Upload correct SQL file

--- Data/ForDBCreation/ward_geo_json.sql
+++ Data/ForDBCreation/ward_geo_json.sql
@@ -1,17 +1,6 @@
-delete from ward_geo_json
-where OBJECTID NOT IN (select OBJECTID from ward_geo_json_attributes);CREATE TABLE `ward_geo_json_attributes` (
+CREATE TABLE `ward_geo_json` (
   `OBJECTID` int NOT NULL,
-  `country` varchar(45) DEFAULT NULL,
-  `wardcode` varchar(10) DEFAULT NULL,
-  `wardname` varchar(100) DEFAULT NULL,
-  `designated` varchar(45) DEFAULT NULL,
-  `status` varchar(45) DEFAULT NULL,
-  `survyear` int DEFAULT NULL,
-  `percancov` float DEFAULT NULL,
-  `standerr` float DEFAULT NULL,
-  `numpts` int DEFAULT NULL,
-  `warea` float DEFAULT NULL,
+  `GEO_JSON` longtext,
   PRIMARY KEY (`OBJECTID`),
-  UNIQUE KEY `OBJECTID_UNIQUE` (`OBJECTID`),
-  KEY `idx_wardcode` (`wardcode`)
+  UNIQUE KEY `OBJECTID_UNIQUE` (`OBJECTID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
